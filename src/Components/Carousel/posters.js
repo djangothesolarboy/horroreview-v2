3b54ba3ba029e5354a80148287a34f4e145b3be3
django <warwickjunglex@gmail.com>
2022-01-12 20:00:05 -0500

    :zap: Changed Film Nav Page

--- src/Components/Carousel/posters.js
+++ src/Components/Carousel/posters.js
@@ -1,3 +1,6 @@
+import { useParams } from 'react-router-dom';
+import { getFilmReview } from '../FilmReviews/filmreviews';
+
 const posters = [
     {
         name: 'malignant',
--- src/Components/FilmReviews/FilmReviewNav.js
+++ src/Components/FilmReviews/FilmReviewNav.js
@@ -5,28 +5,27 @@ import { filmreviews } from './filmreviews';
 import './FilmReviewNav.css';
 
 const FilmReviewList = () => {
-    return (
-        <div className='film-nav-container'>
-            <div className='spoiler-container'>
-                <h2 className='spoiler-warning'>*Spoilers Ahead*</h2>
-            </div>
-            <div className='film-links-container'>
-                {filmreviews.map((film)=> {
-                    return (
-                        <div className='film'>
-                            <NavLink className='review-link' alt={film.alt} to={`/filmreviews/${film.number}`} key={film.number}>
-                                {film.name}
-                            </NavLink><br/>
-                            <Outlet/>
-                            <a href={`/filmreviews/${film.number}`}>
-                                <img className='review-poster' alt={film.alt} src={film.poster}/>
-                            </a>
-                        </div>
-                    )
-                })}
-            </div>
-        </div>
-    )
+	return (
+		<div className='film-nav-container'>
+			<div className='spoiler-container'>
+				<h2 className='spoiler-warning'>*Spoilers Ahead*</h2>
+			</div>
+			<div className='film-links-container'>
+				{filmreviews.map((film)=> {
+					return (
+						<div className='film'>
+							<NavLink className='review-link' alt={film.alt} to={`/filmreviews/${film.number}`} key={film.number}>
+							</NavLink>
+							<Outlet/>
+							<a href={`/filmreviews/${film.number}`}>
+								<img className='review-poster' alt={film.alt} src={film.poster}/>
+							</a>
+						</div>
+					)
+				})}
+			</div>
+		</div>
+	)
 }
 
 export default FilmReviewList;
